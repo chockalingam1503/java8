bba1423dd44e4a699b85858f2a60faf0628567c9
chocku1989 <chocku1989@gmail.com>
2022-04-22 16:47:42 +0530

    Java8 changes

---
+++ src/com/training/filter/TestFilterUsingString.java
@@ -0,0 +1,29 @@
+package com.training.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TestFilterUsingString {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> ar = new ArrayList<String>();
+		ar.add("Chennai");
+		ar.add("Allahabad");
+		ar.add("Coimbatore");
+		
+		List<String> newlist = ar.stream().filter(a->a.startsWith("C")).toList();
+		
+		System.out.println(newlist);
+		
+ar.stream().filter(a->a.startsWith("C")).map(a->a.concat(" : ")).forEach(a->System.out.println(a));
+		
+//		for (String string : newlist) {
+//			
+//		}
+		
+		
+	}
+
+}
